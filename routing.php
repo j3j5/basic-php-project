801db98c91ac553c91ed936c5b7571a4823484ca
Julio Foulquié <jfoulquie@gmail.com>
2015-04-11 13:45:13 +0200

    Remove GET parameters from REQUEST_URI (if present) before doing the routing.

--- routing.php
+++ routing.php
@@ -30,7 +30,12 @@ if(PHP_SAPI == 'cli') {
 
 	$uri_segments = FALSE;
 	if(mb_strlen($_SERVER['REQUEST_URI']) > 1) { // Homepage has '/' as REQUEST_URI
-		$uri_segments = explode('/', $_SERVER['REQUEST_URI']);
+		$uri = $_SERVER['REQUEST_URI'];
+		// Remove GET parameters
+		if(mb_strpos($uri, '?') !== FALSE){
+			$uri = mb_substr($uri, 0, mb_strpos($uri, '?'));
+		}
+		$uri_segments = explode('/', $uri);
 	}
 	if(is_array($uri_segments)) {
 		foreach($url_structure AS $key => $component) {
